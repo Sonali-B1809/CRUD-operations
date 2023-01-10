a9896197ce1512fa22accd642fb7fc092fb7c2b8
Sonali-B1809 <sonali180995@gmail.com>
2023-01-10 12:24:25 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Add from './Components/Add'
+import './index.css'
+
+function App() {
+  return (
+    <div>
+      <Add />
+    </div>
+  )
+}
+
+export default App
